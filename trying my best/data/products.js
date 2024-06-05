a3524bfcd0ead5aa5262fecae0155bf069241cd5
jeypsssss <86237322+jeypsssss@users.noreply.github.com>
2024-06-05 15:59:56 +0800

    Add files via upload

---
+++ trying my best/data/products.js
@@ -0,0 +1,16 @@
+export const products = [{
+    id: 'qwerty1',
+    image: 'product-image/addidas-shoes.jpg',
+    name: 'Addidas Black shoes',
+    price: 300.00
+} , {
+    id: 'qwerty2',
+    image: 'product-image/addidas-daily-2.jpg',
+    name: 'Addidas Daily V2',
+    price: 400.00
+} , {
+    id: 'qwerty3',
+    image: 'product-image/nike-evolution-3.jpg',
+    name: 'Nike Evolution 3',
+    price: 250.00
+}];
